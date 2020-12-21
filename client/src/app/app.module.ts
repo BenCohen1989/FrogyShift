3cf0c4c3f55f843bd40ecc41694f5150af654905
BenCohen1989 <52461746+BenCohen1989@users.noreply.github.com>
2020-12-21 13:51:21 +0200

    add component home and sign in

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './services/auth-interceptor';
 
 
+// paaaaa
+import { NgbPaginationModule, NgbAlertModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SidurListComponent } from './components/sidur-list/sidur-list.component';
+import { WeekCreateComponent } from './components/week-create/week-create.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+
 
 
 @NgModule({
