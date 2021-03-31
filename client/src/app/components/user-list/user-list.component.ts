a16a4719c5b675a4d0ba1cce4adc99fd79caf95d
BenCohen1989 <52461746+BenCohen1989@users.noreply.github.com>
2021-03-31 18:34:50 +0300

    add 2 page for user admin - edite and show all userlist

---
+++ client/src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { SmsService } from 'src/app/services/sms.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users;
+  constructor(public authService: AuthService,
+    public smsService: SmsService) { }
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+  removeUser(user) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this imaginary file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        this.authService.deleteUser(user).subscribe((data: { message }) => {
+          if (data.message == 'success') {
+
+            Swal.fire(
+              'Deleted!',
+              'Your imaginary file has been deleted.',
+              'success'
+            )
+          }
+        })
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+  }
+  sendSms(user) {
+    this.smsService.sendSms(user).subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  getAllUsers() {
+    this.authService.getAllUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
+  disabledUser(user) {
+    if (user.isDisabeld) {
+      user.isDisabeld = false;
+    } else {
+      user.isDisabeld = true;
+    }
+    var userId = user._id;
+    this.authService.UpdateUser(userId, user)
+  }
+}
