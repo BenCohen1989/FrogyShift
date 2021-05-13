3eef55f161a743d38059f145d23e32b6e2e32728
BenCohen1989 <52461746+BenCohen1989@users.noreply.github.com>
2021-05-13 14:35:03 +0300

    before publish1

--- env.js
+++ env.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 if (fs.existsSync('./public')) {
     process.env.JWT = 'secret_this_should_be_longer'
     process.env.NODE_ENV = 'production';
+
+    // process.env.databaseUri = 'mongodb+srv://dbShift:<password>@cluster0.bjj0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     process.env.databaseUri = 'mongodb+srv://dbShift:!QAZ2wsx@cluster0.bjj0e.mongodb.net/frogyShift?retryWrites=true&w=majority';
+    // process.env.databaseUri = 'mongodb+srv://dbShift:!QAZ2wsx@cluster0.bjj0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
     process.env.databaseName = 'production database: shifts2020'; // Database name
     process.env.GMAIL_USER = "shiftboishift@gmail.com";
     process.env.GMAIL_PWS = "Cbehartk1!";
@@ -12,7 +15,7 @@ if (fs.existsSync('./public')) {
 } else {
     process.env.JWT = 'secret_this_should_be_longer'
     process.env.NODE_ENV = 'development';
-
+    // process.env.databaseUri = 'mongodb+srv://dbShift:!QAZ2wsx@cluster0.bjj0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
     process.env.databaseUri = 'mongodb+srv://dbShift:!QAZ2wsx@cluster0.bjj0e.mongodb.net/frogyShift?retryWrites=true&w=majority';
     // process.env.databaseUri = 'mongodb://localhost:27017/name_of_db'; // Databse URI and database name
     // process.env.databaseUri = 'mongodb://shift:!QAZ2wsx@ds048279.mlab.com:48279/shifts2020'; // Databse URI and database name
